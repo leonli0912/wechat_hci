7dc31d87f8ed7cc4317577e803bc0e3ed1aef9e2
I075348 <p.cao@sap.com>
2017-04-06 09:35:17 +0800

    Add some course items, Add the function of back

--- elearning_ui5/WebContent/controller/CourseDetail.controller.js
+++ elearning_ui5/WebContent/controller/CourseDetail.controller.js
@@ -1,9 +1,20 @@
-sap.ui.define(["elearning_ui5/src/js/format/CustomerFormat",'sap/ui/core/mvc/Controller'], function(CustomerFormat,Controller) {
+sap.ui.define(["elearning_ui5/src/js/format/CustomerFormat","sap/ui/core/mvc/Controller","sap/ui/core/routing/History"], function(CustomerFormat,Controller,History) {
 	return Controller.extend("elearning_ui5.controller.CourseDetail", {
 	onInit : function() {
 		
 
-	}
+	},
 	
+	onButtonPress : function () {
+		var oHistory = History.getInstance();
+		var sPreviousHash = oHistory.getPreviousHash();
+
+		if (sPreviousHash !== undefined) {
+			window.history.go(-1);
+		} else {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("joblist", {}, true);
+		}
+	}
 });
 })
