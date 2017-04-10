0ddc64f3cd73102123952fbc505f57f0fd717ba2
Leon Li <leon.li02@sap.com>
2017-04-10 17:47:04 +0800

    copy login view from hana login

---
+++ elearning_ui5/WebContent/controller/Login.controller.js
@@ -0,0 +1,14 @@
+jQuery.sap.require("sap.m.MessageBox");
+sap.ui.define([ "elearning_ui5/src/js/format/CustomerFormat",'sap/ui/core/mvc/Controller' ],function(CustomerFormat, Controller) {
+	return Controller.extend("elearning_ui5.controller.Login",{
+		_logonToken:null,
+		oRouter:null,
+		
+		onInit : function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			if(this._logonToken){
+				
+				this.oRouter.navTo("home");
+			}
+											
+		}});})
