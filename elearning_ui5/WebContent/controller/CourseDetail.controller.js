08f65110a92ba930cc964bb95edf141ec13c002b
Leon Li <leon.li02@sap.com>
2017-04-12 17:26:41 +0800

    use event bus to pass value

--- elearning_ui5/WebContent/controller/CourseDetail.controller.js
+++ elearning_ui5/WebContent/controller/CourseDetail.controller.js
@@ -1,20 +1,29 @@
-sap.ui.define(["elearning_ui5/src/js/format/CustomerFormat","sap/ui/core/mvc/Controller","sap/ui/core/routing/History"], function(CustomerFormat,Controller,History) {
-	return Controller.extend("elearning_ui5.controller.CourseDetail", {
-	onInit : function() {
-		
+sap.ui.define(["elearning_ui5/src/js/format/CustomerFormat", "sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"], function(CustomerFormat, Controller, History) {
+    return Controller.extend("elearning_ui5.controller.CourseDetail", {
+        onInit: function() {
+            var eventBus = sap.ui.getCore().getEventBus();
+            // 1. ChannelName, 2. EventName, 3. Function to be executed, 4. Listener
+            eventBus.subscribe("CourseDetailChannel", "onNavigateEvent", this.onDataReceived, this);
 
-	},
-	
-	onButtonPress : function () {
-		var oHistory = History.getInstance();
-		var sPreviousHash = oHistory.getPreviousHash();
+        },
+        onDataReceived: function(sChannel, sEvent, oData) {
+            if (sChannel === "CourseDetailChannel") {
+                var courseModel = new sap.ui.model.json.JSONModel();
+                courseModel.setData(oData);
+                this.getView().setModel(courseModel);
+            }
 
-		if (sPreviousHash !== undefined) {
-			window.history.go(-1);
-		} else {
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("joblist", {}, true);
-		}
-	}
-});
+        },
+        onButtonPress: function() {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("joblist", {}, true);
+            }
+        }
+    });
 })
