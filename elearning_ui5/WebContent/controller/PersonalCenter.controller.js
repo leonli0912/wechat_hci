fd743bab521f278a3d4b8012e6f54b0cd4cec8ad
leonli0912 <ly95487532@163.com>
2017-04-20 16:29:34 +0800

    Add Person center view

---
+++ elearning_ui5/WebContent/controller/PersonalCenter.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(["elearning_ui5/src/js/format/CustomerFormat",'sap/ui/core/mvc/Controller'], function(CustomerFormat,Controller) {
+	return Controller.extend("elearning_ui5.controller.PersonalCenter", {
+	onInit : function() {
+		
+        
+
+	}
+	
+});
+})
